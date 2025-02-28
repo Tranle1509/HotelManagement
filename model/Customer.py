e999971c859be088a3b9a531e6b68985c5c71d9e
Mhtam1803 <tamnhmk24411e@st.uel.edu.vn>
2025-02-28 22:02:15 +0700

    Delete model/Customer.py

--- model/Customer.py
+++
@@ -1,9 +0,0 @@
-class Customer :
-    def __init__(self,code,name,phone,email):
-        self.code = code
-        self.name=name
-        self.phone=phone
-        self.email=email
-
-    def __str__(self):
-        return f"{self.code}\t{self.name}\t{self.phone}\t{self.email}"
