0900b840f00d86eb246b3c9901737178e948a91a
mhtam1803 <tamnhmk24411e@gmail.com>
2025-03-05 11:35:55 +0700

    .

--- models/Customer.py
+++ models/Customer.py
@@ -1,9 +1,10 @@
 class Customer :
-    def __init__(self,code,name,phone,email):
+    def __init__(self,code,name,phone,email,identity):
         self.code = code
         self.name=name
         self.phone=phone
         self.email=email
+        self.identity=identity
 
     def __str__(self):
-        return f"{self.code}\t{self.name}\t{self.phone}\t{self.email}"
+        return f"{self.code}\t{self.name}\t{self.phone}\t{self.email}\t{self.identity}"
