b71b69114b0a6fde0d0d65a7f85abb47786656a6
mhtam1803 <tamnhmk24411e@gmail.com>
2025-03-05 11:40:45 +0700

    .

--- test/TestCustomer_write.py
+++ test/TestCustomer_write.py
@@ -9,7 +9,7 @@ for i in range(1,20):
     name=f"Customer{i}"
     phone='0' +'9' + ''.join(random.choices('0123456789', k=8))
     email=f"customer{i}@gmail.com"
-    identity='0' ''.join(random.choices('0123456789', k=11))
+    identity='0' +''.join(random.choices('0123456789', k=11))
     c=Customer(code,name,phone,email,identity)
     customers.append(c)
     for c in customers:
