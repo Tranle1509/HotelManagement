fd8970a1f2c99189b72fca3dab0134a6508eac59
DUYEN <duyenthkk24411e@.st.uel.edu.vn>
2025-03-05 10:42:17 +0700

    MainWindow Quản lý phòng

--- ui/MainWindowRoomManagementEx.py
+++ ui/MainWindowRoomManagementEx.py
@@ -2,6 +2,11 @@ from ui.MainWindowRoomManagement import Ui_MainWindow
 
 
 class MainWindowRoomManagementEx(Ui_MainWindow):
+    def __init__(self):
+        self.dc = Dataconnector()
+        self.categories=self.dc.get_all_categories()
+        self.products=self.dc.get_all_products()
+        self.selected_cate=None
     def setupUi(self, MainWindow):
         super().setupUi(MainWindow)
         self.MainWindow=MainWindow
